90765b9dd9a76c4c9f60d3a1d917cb98fa9e095b
riba12331 <fedakandrew@gmail.com>
2020-01-05 19:32:24 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -7,54 +7,6 @@ let lastWorksSlot = document.querySelectorAll(".works_slots a"),
     dots = document.querySelectorAll(".dot");
 
 
-for(let i = 0; i < lastWorksSlot.length; i++){
-    if(i === 0) {
-        lastWorksSlot[i].addEventListener("mouseover",function () {
-            lastWorksSlot[i].style = "background: url('img(hover)/watches(1)(hover).jpg'); transition: 0.2s";
-        });
-        lastWorksSlot[i].addEventListener("mouseout", function () {
-            lastWorksSlot[i].style = "background: url('img/latest_work_icons/watches(1).jpg'); transition: 0.2s";
-        });
-    } else if(i === 1) {
-        lastWorksSlot[i].addEventListener("mouseover", function () {
-            lastWorksSlot[i].style = "background: url('img(hover)/health_pocket(2)(hover).jpg'); transition: 0.2s";
-        });
-        lastWorksSlot[i].addEventListener("mouseout", function () {
-            lastWorksSlot[i].style = "background: url('img/latest_work_icons/health_pocket(2).jpg'); transition: 0.2s";
-        });
-    }
-    else if(i === 2) {
-        lastWorksSlot[i].addEventListener("mouseover",function () {
-            lastWorksSlot[i].style = "background: url('img(hover)/pants(3)(hover).jpg'); transition: 0.2s";
-        });
-        lastWorksSlot[i].addEventListener("mouseout", function () {
-            lastWorksSlot[i].style = "background: url('img/latest_work_icons/pants(3).jpg'); transition: 0.2s";
-        });
-    } else if(i === 3) {
-            lastWorksSlot[i].addEventListener("mouseover", function () {
-                lastWorksSlot[i].style = "background: url('img(hover)/flaga(4)(hover).jpg'); transition: 0.2s";
-            });
-            lastWorksSlot[i].addEventListener("mouseout", function () {
-                lastWorksSlot[i].style = "background: url('img/latest_work_icons/flaga(4).jpg'); transition: 0.2s";
-            });
-    }
-    else if(i === 4) {
-        lastWorksSlot[i].addEventListener("mouseover",function () {
-            lastWorksSlot[i].style = "background: url('img(hover)/wine(5)(hover).jpg'); transition: 0.2s";
-        });
-        lastWorksSlot[i].addEventListener("mouseout", function () {
-            lastWorksSlot[i].style = "background: url('img/latest_work_icons/wine(5).jpg'); transition: 0.2s";
-        });
-    } else if(i === 5){
-        lastWorksSlot[i].addEventListener("mouseover",function () {
-            lastWorksSlot[i].style = "background: url('img(hover)/bag(6)(hover).jpg'); transition: 0.2s";
-        });
-        lastWorksSlot[i].addEventListener("mouseout", function () {
-            lastWorksSlot[i].style = "background: url('img/latest_work_icons/bag(6).jpg'); transition: 0.2s";
-        });
-    }
-}
-
 for(let i = 0; i < latestWorksButtons.length; i++){
     latestWorksButtons[i].addEventListener("click",function () {
         latestWorksButtons.forEach(function(button){
@@ -63,8 +15,8 @@ for(let i = 0; i < latestWorksButtons.length; i++){
                 button.style = "color: #504e4e";
             }
         });
-            latestWorksButtons[i].classList.add('latest_works_buttons_click');
-            latestWorksButtons[i].style = "color:white";
+        latestWorksButtons[i].classList.add('latest_works_buttons_click');
+        latestWorksButtons[i].style = "color:white";
         for(let j = 0; j < latestWorksSlots.length; j++) {
             if(latestWorksButtons[i].classList.contains("all")){
                 latestWorksSlots[j].style = "display:block";
@@ -96,11 +48,11 @@ for(let i = 0; i < latestWorksButtons.length; i++){
         }
     });
 }
+
 membersImage[0].style = "background: url('img/members/JohnDoe.png');";
 membersImage[1].style = "background: url('img/members/AlexaBiru.png');";
 membersImage[2].style = "background: url('img/members/RonobirSing.png');";
 dots[0].classList.add("active");
-
 function swapMember(n){
     for(let i = 0; i < dots.length; i++)
         dots[i].classList.remove("active");
@@ -129,8 +81,6 @@ function swapMember(n){
         dots[1].classList.add("active");
     }
 }
-//<<<<<<< HEAD
-
 setInterval('swapMember(2)', 5000);
 setInterval('swapMember(1)', 10000);
 
